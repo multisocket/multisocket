ae4af9a3255ddf1ff51e9a8df68a47c395adc005
webee.yw <webee.yw@gmail.com>
2019-06-16 02:02:29 +0800

    add errors

---
+++ errors/errors.go
@@ -0,0 +1,18 @@
+package errors
+
+type err string
+
+func (e err) Error() string {
+	return string(e)
+}
+
+// errors
+const (
+	ErrClosed                = err("object is closed")
+	ErrTimeout               = err("operation time out")
+	ErrAddrInUse             = err("address in use")
+	ErrOperationNotSupported = err("operation not supported")
+	ErrBadTransport          = err("invalid or unsupported transport")
+	ErrBadMsg                = err("bad message")
+	ErrMsgTooLong            = err("message is too long")
+)
