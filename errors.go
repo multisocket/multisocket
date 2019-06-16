cd41b50194f044490379562857c77510d0a5f9f8
webee.yw <webee.yw@gmail.com>
2019-06-16 18:24:31 +0800

    fix

--- errors.go
+++ errors.go
@@ -1,14 +1,12 @@
 package multisocket
 
-type err string
-
-func (e err) Error() string {
-	return string(e)
-}
+import (
+	"github.com/webee/multisocket/errors"
+)
 
 // errors
 const (
-	ErrClosed                = err("object is closed")
-	ErrTimeout               = err("operation time out")
-	ErrOperationNotSupported = err("operation not supported")
+	ErrClosed                = errors.ErrClosed
+	ErrTimeout               = errors.ErrTimeout
+	ErrOperationNotSupported = errors.ErrOperationNotSupported
 )
